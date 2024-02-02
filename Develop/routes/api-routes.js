22de6194dbc141da1399435c489ea3490ff88133
David0Perez <147769786+David0Perez@users.noreply.github.com>
2024-02-02 11:49:21 -0500

    .gitignore...

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,3 +1,8 @@
+const router = require('express').Router();
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+
 router.get('/api/notes',async (req, res) => {
     const dbJson = await JSON.parse(dc.readFileSync('./db/db.json', 'utf8'));
     res.json(dbJson);
